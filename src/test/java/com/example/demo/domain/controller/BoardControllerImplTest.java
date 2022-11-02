eb7c85ad6ec96a601cdcf0d262850dedea35e466
채우석 <wochae@wochae-MacBookAir.local>
2022-11-02 18:06:49 +0900

    [feat] 조회수 증가 테스트코드 구현

--- src/test/java/com/example/demo/domain/controller/BoardControllerImplTest.java
+++ src/test/java/com/example/demo/domain/controller/BoardControllerImplTest.java
@@ -1,13 +1,25 @@
 package com.example.demo.domain.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.example.demo.domain.board.Board;
+import com.example.demo.domain.board.BoardRepository;
+import com.example.demo.domain.board.BoardService;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class BoardControllerImplTest {
 
-    @BeforeEach
+    @Autowired
+    BoardService boardService;
+    @Autowired
+    BoardRepository boardRepository;
+
+
 
     @Test
     void saveBoard() {
@@ -20,11 +32,19 @@ class BoardControllerImplTest {
     @Test
     void findOneBoard() {
         // given
+        Board board = new Board();
 
         // when
+        Integer id = boardRepository.save(board).getId();
+        Board origin1 = boardService.findById(id);
+        int beforeView = origin1.getView();
+        boardService.viewBoard(id);
 
-
+        // 왜 origin 으로 따로 값을 넣어줘야 비교할 수 있나
         // then
+        Board origin2 = boardService.findById(id);
+        int afterView = origin2.getView();
+        Assertions.assertThat(afterView).isEqualTo(beforeView + 1);
     }
 
     @Test
